df279925cd23e78cb702e2280a307345af698a68
David Cayo <jdavidcayo@gmail.com>
2022-09-02 02:17:08 -0300

    query's try-catch resolved!

--- src/controllers/task.js
+++ src/controllers/task.js
@@ -2,52 +2,69 @@ import { connect } from '../database';
 
 
 export const getTasks = async (req, res) =>{
-    const conn = await connect();
-    const result = await conn.query( 'SELECT * FROM tasks' )
-    conn.end();
-    res.json(result[0])
+    try{
+        const conn = await connect();
+        const result = await conn.query( 'SELECT * FROM tasks' )
+        conn.end();
+        res.json(result[0])
+    }catch (e){
+        res.json({"message": "query failed!"});
+    }
 };
 
 export const getTask = async (req, res) =>{
-    //Realizar validaciones o implementar un middleware
-    const id = req.params.id;
-    console.log( id );
-    const conn = await connect();
-    const row = await conn.execute( `SELECT * FROM tasks WHERE id=?`, [id]);
-    conn.end();
-    res.json( row[0] );
+    try{
+        const conn = await connect();
+        const row = await conn.execute( `SELECT * FROM tasks WHERE id=?`, [ req.params.id ]);
+        conn.end();
+        const [task] = row[0] ;
+        res.json( task );
+    } catch ( e ){ 
+        res.json({"message": "Unable to find id"});
+    }
 };
 
 export const getTaskCount = async (req, res) =>{
-    const conn = await connect();
-    const cantRegistros = await conn.query(`SELECT COUNT(*) AS nRegistros FROM tasks`);
-    const [nRegistros] = cantRegistros[0];
-    conn.end();
-    res.json(nRegistros);
+    try{
+        const conn = await connect();
+        const cantRegistros = await conn.query(`SELECT COUNT(*) AS nRegistros FROM tasks`);
+        const [nRegistros] = cantRegistros[0];
+        conn.end();
+        res.json(nRegistros);
+    } catch ( e ){
+        res.json({ "message": "Unable to find the total count of items"});
+    }
 };
 
-export const updateTask = async (req, res) =>{ //Realizar update de tarea
-    const conn = await connect();
-    //Realizar validaciones o implementar un middleware
-    const id = req.params.id;
-    const result = await conn.execute(`UPDATE tasks SET title=?, description=? WHERE id=?`, [req.body.title, req.body.description, id]);
-    res.json({"message": "Updated!"});
+export const updateTask = async (req, res) =>{
+    try{
+        const conn = await connect();
+        const result = await conn.execute(`UPDATE tasks SET title=?, description=? WHERE id=?`, [req.body.title, req.body.description, req.params.id]);
+        res.json({"message": "Updated!"});
+    } catch ( e ){
+        res.json({"message": "Update failed!"});
+    }
 };
 
 //  /tasks/:id - deleteTask
 export const deleteTask = async (req, res) =>{
-    const id = req.params.id;
-    const conn = await connect();
-    const result = await conn.execute(`DELETE FROM tasks WHERE id=?`, [ id ]);
-    res.json({"message": "Deleted!"});
+    try{
+        const conn = await connect();
+        const result = await conn.execute(`DELETE FROM tasks WHERE id=?`, [ req.params.id ]);
+        res.json({"message": "Deleted!"});
+    } catch ( e ){
+        res.json({ "message": "Delete failed!"});
+    }
 };
 
 export const saveTask = async(req, res) => {
-    const taskTitle = req.body.title;
-    const taskDescription = req.body.description;
-    const conn = await connect();
-    const result = await conn.execute
-                    (`INSERT INTO tasks (title, description) VALUES (?,?)`, [taskTitle, taskDescription]);                                     
-    conn.end();
-    res.json({"message": `Saved on id: ${result[0].insertId}`});
+    try{
+        const conn = await connect();
+        const result = await conn.execute
+                        (`INSERT INTO tasks (title, description) VALUES (?,?)`, [req.body.title, req.body.description]);                                     
+        conn.end();
+        res.json({"message": `Saved on id: ${result[0].insertId}`});
+    } catch ( e ){
+        res.json({"message": "Save task failed!"});
+    }
 }
